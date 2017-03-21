c8b22d9a545bde19edd920ddcfc7cbb9c72ac410
Isabel Castillo <me@isabelcastillo.com>
2017-03-21 17:10:50 -0400

    delete staff and services pages on uninstall

--- uninstall.php
+++ uninstall.php
@@ -37,10 +37,14 @@ function qbw_uninstall() {
 	// Make sure that the user wants to remove all the data.
 	if ( get_option( 'qbw_delete_data' ) == 'true' ) {
 		global $wpdb;
+		
 		// Delete pages
 		wp_delete_post( get_option( 'qbw_reviews_page_id' ), true );
 		wp_delete_post( get_option( 'qbw_contact_page_id' ), true );
+		wp_delete_post( get_option( 'qbw_staff_page_id' ), true );
+		wp_delete_post( get_option( 'qbw_services_page_id' ), true );
 		wp_delete_post( get_option( 'qbw_about_page_id' ), true );// for backwards compatibilty
+		
 		// get all options with our prefix
 		$query = $wpdb->get_results( "select * from " . $wpdb->options . " where option_name like 'qbw_%'" );
 		// Delete options
